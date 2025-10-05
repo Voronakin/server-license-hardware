acc66fbb1a74668138473486d15771c8c31a2dba
Voron <voronakin@yandex.ru>
2025-10-05 11:45:41 +0300

    clear scope

--- pkg/license/scope.go
+++ pkg/license/scope.go
@@ -5,32 +5,3 @@ type Scope struct {
 	Name        string
 	Description string
 }
-
-var allScopes []Scope
-
-func InitScopes(scopes []Scope) {
-	allScopes = scopes
-}
-
-func GetScopeIds(scopes []Scope) []string {
-	result := make([]string, len(scopes))
-	for i, s := range scopes {
-		result[i] = s.ID
-	}
-
-	return result
-}
-
-func GetScopesByIds(ids []string) []Scope {
-	var result []Scope
-	for _, id := range ids {
-		for _, scope := range allScopes {
-			if scope.ID == id {
-				result = append(result, scope)
-				break
-			}
-		}
-	}
-
-	return result
-}
